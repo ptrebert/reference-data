8418ffa947f95db5543569817922e158c1adf63f
Peter Ebert <pebert@mpi-inf.mpg.de>
2016-11-22 14:36:07 +0100

    ENH: added methods in conversion script to merge UCSC Ensembl annotation files into single BED file

--- scripts/convert_genemodel.py
+++ scripts/convert_genemodel.py
@@ -7,6 +7,7 @@ import sys as sys
 import argparse as argp
 import operator as op
 import traceback as trb
+import collections as col
 
 
 SHOW_PARSER_WARNING = False
@@ -41,6 +42,9 @@ def parse_command_line():
     parser.add_argument('--filter-size', '-fs', type=int, default=0, dest='filtersize')
     parser.add_argument('--filter-type', '-ft', type=str, default='', dest='filtertype')
     parser.add_argument('--show-warnings', '-w', action='store_true', default=False, dest='warnings')
+    parser.add_argument('--ucsc-ensembl', action='store_true', default=False, dest='ucscensembl')
+    parser.add_argument('--ensembl-source', type=str, default='', dest='ensemblsource')
+    parser.add_argument('--ensembl-name', type=str, default='', dest='ensemblname')
     args = parser.parse_args()
     return args
 
@@ -92,6 +96,100 @@ def gff3_column_key(c):
             'score': 4, 'strand': 5, 'phase': 6, 'type': 7,
             'source': 8}
     return cols.get(c, 9)
+
+
+def ensembl_column_key(c):
+    """
+    :param c:
+    :return:
+    """
+    cols = {'chrom': 0, 'start': 1, 'end': 2, 'name': 3,
+            'score': 4, 'strand': 5, 'feature': 6,
+            'feature_type': 7, 'gene_name': 8}
+    return cols.get(c, 9)
+
+
+def read_ensembl_genes(fpath, source, name):
+    """
+    :param fpath:
+    :param source: content of ensemblSource file; contains transcript type info, e.g., protein coding
+    :param name: content of ensemblToGeneName file; contains gene symbol/name info
+    :return:
+    """
+    fieldnames = ['bin', 'name', 'chrom', 'strand', 'txStart', 'txEnd', 'cdsStart', 'cdsEnd',
+                  'exonCount', 'exonStarts', 'exonEnds', 'score', 'name2', 'cdsStartStat',
+                  'cdsEndStat', 'exonFrames']
+    ensembl_genes = col.defaultdict(list)
+    ensembl_rows = []
+    opn, mode = det_open_mode(fpath, True)
+    with opn(fpath, mode) as infile:
+        rows = csv.DictReader(infile, fieldnames=fieldnames, delimiter='\t')
+        for r in rows:
+            assert r['name'] in source, 'Unknown transcript type: {}'.format(r['name'])
+            try:
+                gn = name[r['name']]
+            except KeyError:
+                gn = 'n/a'
+            r['feature_type'] = source[r['name']]
+            r['gene_name'] = gn
+            r['feature'] = 'transcript'
+            r['start'] = r['txStart']
+            r['end'] = r['txEnd']
+            del r['txStart']
+            del r['txEnd']
+            del r['bin']
+            if r['name2'].startswith('ENS'):
+                ensembl_genes[r['name2']].append(r)
+            ensembl_rows.append(r)
+    ensembl_genes = normalize_ensembl_genes(ensembl_genes)
+    ensembl_rows.extend(ensembl_genes)
+    fields = set()
+    for entry in ensembl_rows:
+        fields = fields.union(set(list(entry.keys())))
+    return fields, ensembl_rows
+
+
+def normalize_ensembl_genes(ensgenes):
+    """
+    :param ensgenes:
+    :return:
+    """
+    ens_genes = []
+    for ensid, trans in ensgenes.items():
+        strands = set(t['strand'] for t in trans)
+        assert len(strands) == 1, 'Switching strands: {}'.format(trans)
+        strand = strands.pop()
+        feat_types = set([t['feature_type'] for t in trans])
+        assert len(feat_types) == 1, 'Mixed feature types: {}'.format(trans)
+        feat_type = feat_types.pop()
+        gene_names = set([t['gene_name'] for t in trans])
+        assert len(gene_names) == 1, 'Different gene names: {}'.format(trans)
+        gene_name = gene_names.pop()
+        chrom = trans[0]['chrom']
+        left = str(min([int(t['start']) for t in trans]))
+        right = str(max([int(t['end']) for t in trans]))
+        infos = {'chrom': chrom, 'start': left, 'end': right, 'strand': strand,
+                 'feature_type': feat_type, 'feature': 'gene', 'score': '0',
+                 'name': ensid, 'gene_name': gene_name}
+        ens_genes.append(infos)
+    return ens_genes
+
+
+def read_value_mapping(fpath):
+    """
+    :param fpath:
+    :return:
+    """
+    valmap = dict()
+    opn, mode = det_open_mode(fpath, True)
+    with opn(fpath, mode) as infile:
+        for line in infile:
+            if not line.strip():
+                continue
+            k, v = line.strip().split()
+            assert k not in valmap, 'Duplicate key {} in mapping file {}'.format(k, fpath)
+            valmap[k] = v
+    return valmap
 
 
 def read_gtf_line(line):
@@ -216,6 +314,27 @@ def convert_gff3_file(args):
     return
 
 
+def convert_ensembl_annotation(args):
+    """
+    :param args:
+    :return:
+    """
+    ens_source = read_value_mapping(args.ensemblsource)
+    ens_name = read_value_mapping(args.ensemblname)
+    fields, rows = read_ensembl_genes(args.input, ens_source, ens_name)
+    header = sorted(fields, key=ensembl_column_key)
+    rowbuffer = filter_data_entries(rows, args.filtertype, args.filtersize)
+    opn, mode = det_open_mode(args.output, False)
+    with opn(args.output, mode) as outf:
+        _ = outf.write('#')
+        writer = csv.DictWriter(outf, delimiter='\t', fieldnames=header,
+                                restval='n/a')
+        writer.writeheader()
+        rowbuffer = sorted(rowbuffer, key=lambda x: (x['chrom'], int(x['start']), int(x['end']), x['feature']))
+        writer.writerows(rowbuffer)
+    return
+
+
 def run_genemodel_conversion():
     """
     :return:
@@ -227,6 +346,8 @@ def run_genemodel_conversion():
         convert_gtf_file(args)
     elif '.gff' in args.input:
         convert_gff3_file(args)
+    elif args.ucscensembl:
+        convert_ensembl_annotation(args)
     else:
         raise NotImplementedError
     return 0
