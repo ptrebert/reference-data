512304a0f9bbefcf576792bd15e9bb8d45a7bc31
Peter Ebert <pebert@mpi-inf.mpg.de>
2017-02-01 11:42:31 +0100

    BUG: orthlog task depends on existing gene models - when these are recreated, the function raises; added skip_allowed parameter for this case

--- pipelines/auxmod/orthologs.py
+++ pipelines/auxmod/orthologs.py
@@ -17,6 +17,7 @@ def match_ortholog_files(orthos, genemodels, subsets, outdir, cmd, jobcall):
                   'pig': 'ssc_susScr2_ensembl_v63.bed.gz', 'dog': 'cfa_canFam3_ensembl_v81.bed.gz',
                   'cow': 'Ensembl75_liftOver_Btau_4.6.1_genes.bed.gz'}
     args = []
+    allow_skip = False
     for orth in orthos:
         fp, fn = os.path.split(orth)
         s1, s2 = fn.split('_')[:2]
@@ -25,17 +26,23 @@ def match_ortholog_files(orthos, genemodels, subsets, outdir, cmd, jobcall):
             gm2 = os.path.join(genemodels, spec_model[s2])
             assm1 = spec_assm[s1]
             sub1 = fnm.filter(subsets, '*_{}_*genes.bed.gz'.format(assm1))
-            assert len(sub1) == 1, 'No / ambig. subset file for {}: {}'.format(s1, sub1)
+            if len(sub1) < 1:
+                allow_skip = True
+                continue
+            assert len(sub1) == 1, 'Ambig. subset file for {}: {}'.format(s1, sub1)
             sub1 = sub1[0]
             assm2 = spec_assm[s2]
             sub2 = fnm.filter(subsets, '*_{}_*genes.bed.gz'.format(assm2))
-            assert len(sub2) == 1, 'No / ambig. subset file for {}: {}'.format(s2, sub2)
+            if len(sub2) < 1:
+                allow_skip = True
+                continue
+            assert len(sub2) == 1, 'Ambig. subset file for {}: {}'.format(s2, sub2)
             sub2 = sub2[0]
             outfile = '_'.join([assm1, assm2, 'hcop', 'orthologs']) + '.h5'
             tmp = cmd.format(**{'genemodel1': gm1, 'genemodel2': gm2,
                                 'subset1': sub1, 'subset2': sub2})
             outpath = os.path.join(outdir, outfile)
             args.append([orth, outpath, tmp, jobcall])
-    if orthos:
+    if orthos and not allow_skip:
         assert args, 'No run arguments created for ortholog matching'
     return args
